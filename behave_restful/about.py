40ae6104994246c92b797c332e57a01b7b432270
Isaac Rodriguez <isaac_rodriguez@live.com>
2018-01-28 14:22:42 -0500

    Adds About Module

---
+++ behave_restful/about.py
@@ -0,0 +1,27 @@
+"""
+Provides properties with information about ``behave_restful``.
+"""
+
+project = 'behave-restful'
+description = 'Implements Gherking language for REST services.'
+long_description = '''
+Behave Restful implements a language based on Gherkin that allows to test
+REST services using the behave python tool.
+'''
+copyright = '2017 Behave Restful'
+author = 'Isaac Rodriguez'
+author_email = 'isaac_rodriguez@live.com'
+maintainer = 'Isaac Rodriguez'
+
+version = '0.1'
+release = '0.1.0'
+
+classifiers = [
+    'Development Status :: 1 - Planning',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+]
+
+keywords = 'behave gherking rest test bdd'

c38ce906b003401440182deb5e5a81735231778a
Isaac Rodriguez <isaac_rodriguez@live.com>
2018-02-10 14:00:36 -0500

    List dependencies in setup.py

--- behave_restful/about.py
+++ behave_restful/about.py
@@ -14,7 +14,7 @@ author_email = 'isaac_rodriguez@live.com'
 maintainer = 'Isaac Rodriguez'
 
 version = '0.1'
-release = '0.1.2'
+release = '0.1.3'
 
 classifiers = [
     'Development Status :: 1 - Planning',

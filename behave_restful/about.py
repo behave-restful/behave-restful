10d89d7f5ee6e0dddd616f389ce28874d2313831
Isaac Rodriguez <isaac_rodriguez@live.com>
2018-02-12 18:33:52 -0500

    Bumped version to 0.1.4

--- behave_restful/about.py
+++ behave_restful/about.py
@@ -14,7 +14,7 @@ author_email = 'isaac_rodriguez@live.com'
 maintainer = 'Isaac Rodriguez'
 
 version = '0.1'
-release = '0.1.3'
+release = '0.1.4'
 
 classifiers = [
     'Development Status :: 1 - Planning',

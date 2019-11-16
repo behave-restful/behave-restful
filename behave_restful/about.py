08a126bbdc28a709e32de8515b4494e0d2455683
Isaac Rodriguez <isaac_rodriguez@live.com>
2019-11-15 20:19:57 -0500

    Migrated to jsonpath module

--- behave_restful/about.py
+++ behave_restful/about.py
@@ -3,8 +3,8 @@ Provides properties with information about ``behave_restful``.
 """
 
 project = 'behave-restful'
-version = '0.1'
-release = '0.1.12'
+version = '0.2'
+release = '0.2.0'
 description = 'Implements Gherking language for REST services.'
 copyright = '2017 Abantos'
 author = 'Isaac Rodriguez'
